ebab1e5da137eaf8e38ce02f9bc2642befb86eee
chiqina <1078720359@qq.com>
2025-07-24 18:26:38 +0800

    mall初次提交

---
+++ mall-admin/src/router/index.js
@@ -0,0 +1,21 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'manager',
+    component:()=>import("../views/Manager.vue")
+    // component: ()=>import("../views/HomeView.vue")
+  }
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+export default router
---
+++ mall-admin/src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    首页123
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeView',
+}
+</script>
